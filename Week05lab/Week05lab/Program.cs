ca307cfa05257d5963b055659eaa59c0665d320f
minwichita <59030206@kmitl.ac.th>
2017-09-08 14:35:02 +0700

    Edited-while

--- Week05lab/Week05lab/Program.cs
+++ Week05lab/Week05lab/Program.cs
@@ -11,38 +11,22 @@ namespace Week05lab
         static void Main(string[] args)
 
         {
-            Console.Write("Input your grade (A, B, C, D or F) : ");
-            string gradeString = Console.ReadLine();
-            string message;
-            switch (gradeString.ToUpper())
+            int n = 1;
+            while (n < 6)
             {
-                case "A":
-                    message = "Excellent";
-                    break;
-                case "B":
-                    message = "Good";
-                    break;
-                case "C":
-                    message = "Cool";
-                    break;
-                case "D":
-                    message = "Try";
-                    break;
-                case "F":
-                    message = "Get out!!";
-                    break;
-                default:
-                    message = "Incorrect grade";
-                    break;
+                Console.WriteLine("Current value of n is {0}", n);
+                n++;
+
+
+
+
+
             }
-            Console.WriteLine(message);
-
-
-
-
         }
     }
-        }
+}
+
+
   
 
 

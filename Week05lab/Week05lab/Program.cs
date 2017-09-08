f0e37f166c445b767de7b4cf179c133e5c2dbbcf
minwichita <59030206@kmitl.ac.th>
2017-09-08 11:30:11 +0700

    Edited-Goto2

--- Week05lab/Week05lab/Program.cs
+++ Week05lab/Week05lab/Program.cs
@@ -11,18 +11,19 @@ namespace Week05lab
         static void Main(string[] args)
         {
             Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
+
+            goto line4;
             line4:
             Console.WriteLine("Line 4");
             Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
+            
+            goto line2;
+            line2:
+            Console.WriteLine("Line 2");
+
+            goto line9;
+            line9:
             Console.WriteLine("Line 9");
-            line10:
-            Console.WriteLine("Line 10");
 
         }
     }
